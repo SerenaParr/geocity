ccfdeaa92bdf7bdabc84d086e0eb2014d2ae7bd6
Isabel Donlon <imd244@nyu.edu>
2016-10-30 15:39:55 -0400

    Merged Scenes

---
+++ geocity_home/Assets/Scripts/SimpleTrigerableFunctions.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleTrigerableFunctions : MonoBehaviour {
+
+	public GameObject activatableComponent;
+	AudioSource audio;
+
+	void Start() {
+		audio = GetComponent<AudioSource> ();
+	}
+
+	public void turnOnGO(){
+//		activatableComponent.SetActive (true);
+		((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+		audio.Play ();
+
+	}
+
+	public void turnOffGO(){
+//		activatableComponent.SetActive (false);
+		((MovieTexture)GetComponent<Renderer>().material.mainTexture).Pause();
+
+	}
+
+}
